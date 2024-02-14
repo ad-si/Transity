3966f035e23a7e8b13ff1802202871f83002b62d
Adrian Sieber <mail@adriansieber.com>
2024-02-14 15:46:15 +0000

    Add first draft of flake.nix file

---
+++ flake.nix
@@ -0,0 +1,68 @@
+# WARNING:
+# This is still a work in progress and not yet fully functional.
+{
+  description = "Transity";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    purescript-overlay = {
+      url = "github:thomashoneyman/purescript-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        config = { };
+        overlays = builtins.attrValues self.overlays;
+      });
+    in {
+      overlays = {
+        purescript = inputs.purescript-overlay.overlays.default;
+      };
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system}; in {
+          default = pkgs.buildNpmPackage {
+            name = "transity";
+            buildInputs = [ nixpkgsFor.${system}.nodejs_20 ];
+            src = ./.;
+            npmDepsHash = "sha256-yBjzdVe2nNGT3ACXhz/ZsOCm7d4ELvHGed9ZD410LAY=";
+            npmBuild = "make bundle";
+            installPhase = ''
+              mkdir $out
+              cp index.js $out
+            '';
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system}; in {
+          default = pkgs.mkShell {
+            name = "transity";
+            inputsFrom = builtins.attrValues self.packages.${system};
+            buildInputs = with pkgs; [
+              nodejs_20
+              watchexec
+
+              # TODO: Currently still installed via npm
+              # purs
+              # purs-backend-es
+              # purs-tidy-bin.purs-tidy-0_10_1
+              # spago-unstable
+            ];
+          };
+        });
+  };
+}
